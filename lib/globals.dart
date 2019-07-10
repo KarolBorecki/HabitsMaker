cb0796b0f3f00018b1f8f1794e5251afdd9cb8a6
karolborecki <karol.borecki@onet.eu>
2019-07-10 20:46:15 +0200

    Colors has been updated

--- lib/globals.dart
+++ lib/globals.dart
@@ -13,10 +13,20 @@ var appColors = [
   Color.fromRGBO(255, 0, 102, 1.0),
   Color.fromRGBO(204, 51, 255, 1.0),
   Color.fromRGBO(102, 102, 255, 1.0),
-  Color.fromRGBO(0, 255, 0, 1.0),
-  Color.fromRGBO(255, 255, 0, 1.0),
+  Color.fromRGBO(46, 204, 64, 1.0),
+  Color.fromRGBO(255, 230, 50, 1.0),
   Color.fromRGBO(255, 0, 255, 1.0),
-  Color.fromRGBO(255, 255, 255, 1.0),
+  Color.fromRGBO(57, 121, 150, 1.0),
+  Color.fromRGBO(49, 79, 112, 1.0),
+  Color.fromRGBO(246, 114, 128, 1.0),
+  Color.fromRGBO(69, 173, 168, 1.0),
+  Color.fromRGBO(157, 224, 173, 1.0),
+  Color.fromRGBO(255, 65, 54, 1.0),
+  Color.fromRGBO(255, 133, 27, 1.0),
+  Color.fromRGBO(177, 13, 201, 1.0),
+  Color.fromRGBO(0, 116, 217, 1.0),
+  Color.fromRGBO(255, 220, 0, 1.0),
+  Color.fromRGBO(240, 18, 190, 1.0),
 ];
 
 var weekDays = [
